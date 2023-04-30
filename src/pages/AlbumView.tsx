b7097ed1006a00e1987cf0b43e3b5a0aba8a1a74
Ksenia <ksenia.ivanovva@gmail.com>
2023-04-30 12:18:58 +0200

    feat: created AlbumView component

--- src/pages/AlbumView.tsx
+++ src/pages/AlbumView.tsx
@@ -1,5 +1,26 @@
+import { useLoaderData, useParams } from "react-router";
+import { Album } from "../components/Album";
+import { Loader } from "../loaders/albumLoader";
+import { myAlbum } from "../components/models/MyAlbum";
+import { Navbar } from "../components/Navbar";
+
 
 
 export const AlbumView = () => {
+    const params = useParams();
+    const loadedData = useLoaderData() as Loader;
+
+    const currentAlbum = loadedData.albums.find((album: myAlbum) => album.id === params.id);
+
+    if (currentAlbum === undefined) {
+        return <h2>Choose an album!</h2>;
+    } else {
+        return (
+            <>
+                <Navbar></Navbar>
+                <Album {...currentAlbum} showFullInfo={true}></Album>
+            </>
+        );
+    }
 
 }
